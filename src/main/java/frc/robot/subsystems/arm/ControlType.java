02cb33ed0ea1267146a1be6319132e892ec0c795
Michael A. Hill <michael.hill@innovators.lan>
2025-01-20 18:59:53 -0500

    test swerve drivebase

--- src/main/java/frc/robot/subsystems/arm/ControlType.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.arm;
-
-public class ControlType {
-
-    public static com.revrobotics.ControlType kPosition;
-
-}
--- src/main/java/frc/robot/subsystems/elevator/MotorType.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.elevator;
-
-public class MotorType {
-
-    public static Object kBrushless;
-    
-}
