d6a0d370fbad20d17fb7ff65311e49326ee5514a
Owen Leber <owen.m.leber@gmail.com>
2025-01-21 19:20:32 -0500

    finsihed declaring solenoids and sparkmax's

--- src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
@@ -3,17 +3,21 @@
 
 package frc.robot.subsystems;
 
-import frc.robot.Constants;
-
-import com.revrobotics.spark.SparkMax;
-
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystemCopy extends SubsystemBase {
-  final private PWMSparkMax m_rightIntakeMotor = new PWMSparkMax(1); 
+
+    
+        @SuppressWarnings("unused")
+        final private PWMSparkMax m_rightIntakeMotor = new PWMSparkMax(0); 
+        @SuppressWarnings("unused")
+        final private PWMSparkMax m_leftIntakeMotor = new PWMSparkMax(0);
+        @SuppressWarnings("unused")
+        final private DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
+        @SuppressWarnings("unused")
+        final private DoubleSolenoid m_rightDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
+
 }
