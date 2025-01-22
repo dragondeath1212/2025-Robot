33867cb75510c7a18a24265978e5e9a042af01fd
Owen Leber <owen.m.leber@gmail.com>
2025-01-21 20:18:44 -0500

    update IntakeSubsystemCopy to use SparkMax motors and set solenoid states

--- src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
@@ -1,9 +1,10 @@
-// Author: UMN Robotics Ri3d
-// Last Updated : January 2024
+// Author: Innovators robotics
+// Last Updated : today
 
 package frc.robot.subsystems;
-
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,9 +13,9 @@ public class IntakeSubsystemCopy extends SubsystemBase {
 
     
         @SuppressWarnings("unused")
-        final private PWMSparkMax m_rightIntakeMotor = new PWMSparkMax(0); 
+        final private SparkMax m_rightIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless); 
         @SuppressWarnings("unused")
-        final private PWMSparkMax m_leftIntakeMotor = new PWMSparkMax(0);
+        final private SparkMax m_leftIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless);
         @SuppressWarnings("unused")
         final private DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
         @SuppressWarnings("unused")
@@ -22,6 +23,10 @@ public class IntakeSubsystemCopy extends SubsystemBase {
         public IntakeSubsystemCopy(){
             m_rightIntakeMotor.setInverted(false);
             m_leftIntakeMotor.setInverted(false);
+
+
+            m_leftDoubleSolenoid.set(Value.kOff);
+            m_rightDoubleSolenoid.set(Value.kOff);
 
 
 
