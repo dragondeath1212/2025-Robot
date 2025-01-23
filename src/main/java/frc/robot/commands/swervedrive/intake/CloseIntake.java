9f13ce229016205f1d1d3e65dd1f240a0e522c84
Owen Leber <owen.m.leber@gmail.com>
2025-01-22 21:10:04 -0500

    started close intake command

---
+++ src/main/java/frc/robot/commands/swervedrive/intake/CloseIntake.java
@@ -0,0 +1,19 @@
+
+
+package frc.robot.commands.swervedrive.intake;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystemCopy; 
+
+public class CloseIntake extends Command {
+private final IntakeSubsystemCopy intakeSubsystem;
+
+
+
+
+
+public CloseIntake(IntakeSubsystemCopy intakeSubsystem) {
+	this.intakeSubsystem = intakeSubsystem;
+}
+
+
+}
