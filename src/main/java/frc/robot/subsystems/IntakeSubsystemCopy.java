f1a3b2c6008d67faa708a732c502c7819d1b04ea
Owen Leber <owen.m.leber@gmail.com>
2025-01-22 19:31:51 -0500

    add extender solenoid and  methods to IntakeSubsystemCopy

--- src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
@@ -11,15 +11,21 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IntakeSubsystemCopy extends SubsystemBase {
 
     
-        @SuppressWarnings("unused")
+    
         final private SparkMax m_rightIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless); 
-        @SuppressWarnings("unused")
         final private SparkMax m_leftIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless);
-        @SuppressWarnings("unused")
+    
         final private DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
-        @SuppressWarnings("unused")
         final private DoubleSolenoid m_rightDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
+        final private DoubleSolenoid m_extenderDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0,0);
       boolean IsIntakeClosed = false;
+
+      void extendIntake(){
+            m_extenderDoubleSolenoid.set(Value.kForward);
+      }
+      void returnIntake(){
+        m_extenderDoubleSolenoid.set(Value.kReverse);
+  }
 
 
         void closeIntake(){
