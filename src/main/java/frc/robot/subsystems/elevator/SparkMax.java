2807bf585b05683bfe077cbaeac1d85084215a31
Charlie Guy <charliegamerguy@gmail.com>
2025-01-23 18:59:19 -0500

    refactor SparkMax: add static REVLibError constant and clean up unused fields

--- src/main/java/frc/robot/subsystems/elevator/SparkMax.java
+++ src/main/java/frc/robot/subsystems/elevator/SparkMax.java
@@ -4,63 +4,64 @@ import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
 
 public class SparkMax {
-    @SuppressWarnings("unused")
-    private int channel;
-    @SuppressWarnings("unused")
-    private MotorType motorType;
-    @SuppressWarnings("unused")
-    private int currentLimit;
-    @SuppressWarnings("unused")
-    private boolean inverted;
-    @SuppressWarnings("unused")
-    private boolean brakeMode;
-    @SuppressWarnings("unused")
-    private double rampRate;
-    private double speed;
-    private double position;
-    private double current;
-    private double temperature;
-
-    public SparkMax(int channel) {
-        this.channel = channel;
-    }
-
-    public void setMotorType(MotorType motorType) {
-        this.motorType = motorType;
-    }
-
-    public void setCurrentLimit(int currentLimit) {
-        this.currentLimit = currentLimit;
-    }
-
-    public void setInverted(boolean inverted) {
-        this.inverted = inverted;
-    }
-
-    public void setBrakeMode(boolean brakeMode) {
-        this.brakeMode = brakeMode;
-    }
-
-    public void setRampRate(double rampRate) {
-        this.rampRate = rampRate;
-    }
-
-    public RelativeEncoder getEncoder() {
-        return new RelativeEncoder() {
-            @Override
-            public double getPosition() {
-                return position;
-            }
-
-            @Override
-            public double getVelocity() {
-                return speed;
-            }
-
-            @Override
-            public REVLibError setPosition(double position) {
-                SparkMax.this.position = position;
-                                return null;
+    public static final REVLibError REVLibError = null;
+        @SuppressWarnings("unused")
+        private int channel;
+        @SuppressWarnings("unused")
+        private MotorType motorType;
+        @SuppressWarnings("unused")
+        private int currentLimit;
+        @SuppressWarnings("unused")
+        private boolean inverted;
+        @SuppressWarnings("unused")
+        private boolean brakeMode;
+        @SuppressWarnings("unused")
+        private double rampRate;
+        private double speed;
+        private double position;
+        private double current;
+        private double temperature;
+    
+        public SparkMax(int channel) {
+            this.channel = channel;
+        }
+    
+        public void setMotorType(MotorType motorType) {
+            this.motorType = motorType;
+        }
+    
+        public void setCurrentLimit(int currentLimit) {
+            this.currentLimit = currentLimit;
+        }
+    
+        public void setInverted(boolean inverted) {
+            this.inverted = inverted;
+        }
+    
+        public void setBrakeMode(boolean brakeMode) {
+            this.brakeMode = brakeMode;
+        }
+    
+        public void setRampRate(double rampRate) {
+            this.rampRate = rampRate;
+        }
+    
+        public RelativeEncoder getEncoder() {
+            return new RelativeEncoder() {
+                @Override
+                public double getPosition() {
+                    return position;
+                }
+    
+                @Override
+                public double getVelocity() {
+                    return speed;
+                }
+    
+                @Override
+                public REVLibError setPosition(double position) {
+                    SparkMax.this.position = position;
+                                    return (REVLibError);
             }
         };
     }
