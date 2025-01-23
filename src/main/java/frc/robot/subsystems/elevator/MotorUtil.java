b0d4b2b1353513249cd48a1b01f9cc018cf0fa94
Charlie Guy <charliegamerguy@gmail.com>
2025-01-23 18:55:12 -0500

    refactor MotorUtil: update createSparkMAX method to accept currentLimit and remove unsupported overloads

--- src/main/java/frc/robot/subsystems/elevator/MotorUtil.java
+++ src/main/java/frc/robot/subsystems/elevator/MotorUtil.java
@@ -2,15 +2,19 @@ package frc.robot.subsystems.elevator;
 
 public class MotorUtil {
 
+    public static SparkMax createSparkMAX(int elevatorMotorId, MotorType motorType, int currentLimit, boolean inverted,
+            boolean brakeMode, double rampRate) {
+        SparkMax sparkMax = new SparkMax(elevatorMotorId);
+        sparkMax.setMotorType(motorType);
+        sparkMax.setCurrentLimit(currentLimit);
+        sparkMax.setInverted(inverted);
+        sparkMax.setBrakeMode(brakeMode);
+        sparkMax.setRampRate(rampRate);
+        return sparkMax;
+    }
 
     public static SparkMax createSparkMAX(int elevatorMotorId, MotorType kbrushless, String neoCurrentLimit,
             boolean inverted, boolean brakeMode, double rampRate) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'createSparkMAX'");
-    }
-
-    public static SparkMax createSparkMAX(int elevatorMotorId, MotorType kbrushless, int i, boolean inverted,
-            boolean brakeMode, double rampRate) {
         
         throw new UnsupportedOperationException("Unimplemented method 'createSparkMAX'");
     }
