7deecc15dfc4c6cc8b28c2cfef220f19812bdfcf
Orion Little <dragonboy022222@gmail.com>
2025-01-22 20:09:03 -0500

    feat: add WPILib dependency and initialize kPosition variable; remove unused MotorType class

--- src/main/java/frc/robot/subsystems/arm/ControlType.java
+++ src/main/java/frc/robot/subsystems/arm/ControlType.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems.arm;
 
 public class ControlType {
 
-    public static com.revrobotics.ControlType kPosition;
+    public static com.revrobotics.ControlType kPosition = com.revrobotics.ControlType.kPosition; // Initialize the variable
 
 }
