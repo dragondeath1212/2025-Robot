c90de14c0c2877c8bdd85e0d4dff0f97d5a73e12
Owen Leber <owen.m.leber@gmail.com>
2025-01-23 20:08:47 -0500

    Update joystick GUID and adjust intake subsystem motor configurations

--- src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystemCopy.java
@@ -13,11 +13,11 @@ public class IntakeSubsystemCopy extends SubsystemBase {
     
     
         final private SparkMax m_rightIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless); 
-        final private SparkMax m_leftIntakeMotor = new SparkMax(0, SparkMax.MotorType.kBrushless);
+        final private SparkMax m_leftIntakeMotor = new SparkMax(1, SparkMax.MotorType.kBrushless);
     
-        final private DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
-        final private DoubleSolenoid m_rightDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
-        final private DoubleSolenoid m_extenderDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0,0);
+        final private DoubleSolenoid m_leftDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 3);
+        final private DoubleSolenoid m_rightDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 4);
+        final private DoubleSolenoid m_extenderDoubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 2,5);
       boolean IsIntakeClosed = false;
 
       public void extendIntake(){
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.elevator;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
 public class ElevatorSubsystem extends SubsystemBase {
