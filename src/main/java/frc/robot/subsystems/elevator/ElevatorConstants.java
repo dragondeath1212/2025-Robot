3a401bb1a9c8259d319f04b3643bb9b6b02818d4
Charlie Guy <charliegamerguy@gmail.com>
2025-01-25 10:53:08 -0500

    refactor ElevatorConstants and simgui-ds: update elevator motor ID and add new keyboard GUID

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -4,7 +4,7 @@ public final class ElevatorConstants {
 
     // Add your constants here
 
-    public static final int ELEVATOR_MOTOR_ID = 13; // Replace 1 with the actual motor ID
+    public static final int ELEVATOR_MOTOR_ID = 16; // Replace 1 with the actual motor ID
     public static final String METERS_PER_REVOLUTION = null;
     
 }
