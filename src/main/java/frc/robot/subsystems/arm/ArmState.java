45c71afcc53a6fceb75e5059709b8420cd1109a6
Orion Little <dragonboy022222@gmail.com>
2025-02-03 18:21:15 -0500

    fix: remove unused suppress warnings for joint position and velocity in ArmState

--- src/main/java/frc/robot/subsystems/arm/ArmState.java
+++ src/main/java/frc/robot/subsystems/arm/ArmState.java
@@ -4,16 +4,14 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public class ArmState {
-    @SuppressWarnings("unused")
     private Angle m_jointPosition;
-    @SuppressWarnings("unused")
     private AngularVelocity m_jointVelocity;
-
+    
     public ArmState(Angle jointPosition, AngularVelocity jointVelocity) {
         m_jointPosition = jointPosition;
         m_jointVelocity = jointVelocity;
     }
-
+    
     public void setArmState(Angle jointPosition, AngularVelocity jointVelocity) {
         m_jointPosition = jointPosition;
         m_jointVelocity = jointVelocity;
