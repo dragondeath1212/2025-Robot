6fc3e6930fd15ef24e2a316fbc851594ce73735d
Charlie Guy <charliegamerguy@gmail.com>
2025-02-12 18:38:35 -0500

    refactor: remove SparkMax class and implement ClimbSubsystem with ClimbSpark motor controller

---
+++ src/main/java/frc/robot/subsystems/climb/ClimbMotor.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.climb;
+
+import frc.robot.subsystems.elevator.ElevatorConstants;
+
+
+public class ClimbMotor {
+    public class MotorUtil {
+
+    public static ClimbSpark createSparkMAX(int climbMotorId, ElevatorConstants motorType, int currentLimit, boolean inverted,
+            boolean brakeMode, double rampRate) {
+        ClimbSpark sparkMax = new ClimbSpark();
+        return sparkMax;
+    }
+}
+}
