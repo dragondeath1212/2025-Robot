29d32035d689f7d5ad9dd72ce8037fcbc25c86a2
Owen Leber <owen.m.leber@gmail.com>
2025-02-12 21:07:07 -0500

    fixed deativate intake, need to fix other commands too

--- src/main/java/frc/robot/commands/intake/DeactivateIntake.java
+++ src/main/java/frc/robot/commands/intake/DeactivateIntake.java
@@ -4,28 +4,39 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem; 
 
 public class DeactivateIntake extends Command {
-private final IntakeSubsystem intakeSubsystem;
-
+private final IntakeSubsystem m_intakeSubsystem;
+private boolean m_finished = true;
 
 
 
 
 public DeactivateIntake(IntakeSubsystem intakeSubsystem) {
 
-this.intakeSubsystem = intakeSubsystem;
+this.m_intakeSubsystem = intakeSubsystem;
 
 
 }
 
 public void initialize() {
-    intakeSubsystem.stopIntake();
-   
-    intakeSubsystem.returnIntake();
+
 }
 public void execute() {
-    
+    if (!m_finished == true) {
+        m_intakeSubsystem.stopIntake();
+   
+        m_intakeSubsystem.returnIntake();
+        m_finished = true;
+    }
+
+
+
 }
 public void end() {
 
+
+}
+
+public boolean isfinished() {
+    return m_finished;
 }
 }
