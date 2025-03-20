7bf9edfdaff2332c2d991cb1374ff5a1f17303ac
Kyle <kyle.leber@gmail.com>
2025-03-19 20:45:20 -0400

    Fixed shoulder lock-out value so its less likely to accidentally fall into it

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -238,7 +238,7 @@ public class Arm extends SubsystemBase {
             voltage = -1 * voltage;
         }
 
-        if (getShoulderPosition().in(Rotations) < 0.05 && getShoulderPosition().in(Rotations) > -0.46) {
+        if (getShoulderPosition().in(Rotations) < 0.05 && getShoulderPosition().in(Rotations) > -0.475) {
             if (!shoulderAtSetpoint) {
                 //leave voltage alone
             } else {
